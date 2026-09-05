c3c4bcc1685492b8756d36e0cebdade2cc6dc6b6
Hugo Chimendes <hugo.leite1201@gmail.com>
2026-09-05 20:06:31 -0300

    feat: removendo o MainLayout da pasta

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {Outlet} from "react-router";
-import MainLayout from "./layouts/MainLayout/MainLayout";
+import MainLayout from "./layouts/MainLayout";
 
 export default function App() {
   return (
