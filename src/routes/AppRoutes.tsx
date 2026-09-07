94087c890e6777a5da47914fc50312dbc042f11c
velascoguzmanvictorhugo-ui <velascoguzmanvictorhugo@gmail.com>
2026-09-07 00:53:31 -0300

    feat: atualizacao ErrorBoundary e envolve arvore de rotas

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,18 +2,21 @@ import { Route, Routes } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import ErrorBoundary from "../components/Error/ErrorBoundary";
 
+function Bug() {
+  return (null as any).texto;
+}
 export default function AppRoutes() {
   return (
     <ErrorBoundary>
       <Routes>
-        <Route element={ <MainLayout /> }>
-          <Route path="/" element={<h1>Home</h1>} />
+        <Route element={<MainLayout />}>
+        <Route path="/" element={<Bug />} />
           <Route path="/locais" element={<h1>Locais</h1>} />
           <Route path="/locais/:id" element={<h1>Detalhe do Local</h1>} />
-          <Route path="/cadastrar" element={<h1>Cadastro</h1>} />
+          <Route path="/cadastrar" element={<h1>Cadastrar</h1>} />
           <Route path="/sobre" element={<h1>Sobre</h1>} />
         </Route>
       </Routes>
     </ErrorBoundary>
   );
-};
+}
