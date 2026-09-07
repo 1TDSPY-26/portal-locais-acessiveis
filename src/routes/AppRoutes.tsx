3481a7fe4298184ebebe21ffed878bd1e2512912
velascoguzmanvictorhugo-ui <velascoguzmanvictorhugo@gmail.com>
2026-09-07 00:09:24 -0300

    feat: implementacao ErrorBoundary e envolve arvore de rotas

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,16 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
+import ErrorBoundary from "../components/Error/ErrorBoundary";
 
 export default function AppRoutes() {
   return (
-    <Routes>
-      <Route element={ <MainLayout /> }>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/locais" element={<h1>Locais</h1>} />
-        <Route path="/locais/:id" element={<h1>Detalhe do Local</h1>} />
-        <Route path="/cadastrar" element={<h1>Cadastro</h1>} />
-        <Route path="/sobre" element={<h1>Sobre</h1>} />
-      </Route>
-    </Routes>
+    <ErrorBoundary>
+      <Routes>
+        <Route element={ <MainLayout /> }>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/locais" element={<h1>Locais</h1>} />
+          <Route path="/locais/:id" element={<h1>Detalhe do Local</h1>} />
+          <Route path="/cadastrar" element={<h1>Cadastro</h1>} />
+          <Route path="/sobre" element={<h1>Sobre</h1>} />
+        </Route>
+      </Routes>
+    </ErrorBoundary>
   );
 };
