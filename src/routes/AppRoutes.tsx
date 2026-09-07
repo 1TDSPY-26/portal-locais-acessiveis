c7172f98d823ca233058ff6b47a547898266dd41
alvesdemacedoenzo-beep <alvesdemacedoenzo@gmail.com>
2026-09-07 18:48:00 -0300

    feat: adiciona pagina para enderecos nao encontrados

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
+import NotFound from "../pages/NotFound";
 
 export default function AppRoutes() {
   return (
@@ -10,6 +11,7 @@ export default function AppRoutes() {
         <Route path="/locais/:id" element={<h1>Detalhe do Local</h1>} />
         <Route path="/cadastrar" element={<h1>Cadastro</h1>} />
         <Route path="/sobre" element={<h1>Sobre</h1>} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
