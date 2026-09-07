1e937c33d4e202fded5d763b9aa5a986ace55dde
juhfreitaas <juliafreitassantos56@gmail.com>
2026-09-06 22:44:09 -0300

    feat: adicionando links de navegação ao header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,17 @@
-export default function Cabecalho(){
+import { Link } from "react-router-dom";
 
-   return(
-      <header>
-         <h1>Cabeçalho</h1>
-      </header>
-   )
+export default function Cabecalho() {
+  return (
+    <header className="bg-black text-white">
+      <h1>Cabecalho</h1>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/locais">Locais</Link></li>
+          <li><Link to="/cadastro">Cadastro</Link></li>
+          <li><Link to="/sobre">Sobre</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
