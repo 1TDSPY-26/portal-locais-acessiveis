71a3633e78169e9d148e8d70e5bf1555b816afb6
PietroSantos <pietro.oliveirabr@gmail.com>
2026-09-17 22:34:50 -0300

    feat: documentar recursos e limites de acessibilidade

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -7,6 +7,7 @@ import Home from "../pages/Home/Home";
 import PaginaLocais from "../pages/Locations/PaginaLocais";
 // import Locais from "../pages/Locais/Locais";
 import Sobre from "../pages/Sobre/Sobre";
+import Acessibilidade from "../pages/Acessibilidade/Acessibilidade";
 
 export default function AppRoutes() {
   return (
@@ -23,7 +24,7 @@ export default function AppRoutes() {
           <Route path="/cadastro" element={<h1>Cadastro</h1>} />
           <Route path="/cadastrar" element={<Navigate to="/cadastro" replace />} />
           <Route path="/sobre" element={<Sobre />} />
-          <Route path="/acessibilidade" element={<h1>Acessibilidade</h1>} />
+          <Route path="/acessibilidade" element={<Acessibilidade />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
